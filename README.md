3a9b4b4f191068ebcfb1ed0c18a7bf4ac92e7f7c
Goivnd Kumar Yadav <robertgovind@gmail.com>
2025-01-09 18:19:35 +0545

    hosted-website source code

---
+++ README.md
@@ -0,0 +1,46 @@
+# Portfolio Website
+
+Welcome to my portfolio website! This repository contains the source code for my personal website, showcasing my projects, skills, and professional experience. It's live [here](https://www.govindyadav.com.np)
+
+## Overview
+
+This portfolio website is designed to:
+- Highlight my skills and achievements.
+- Showcase my projects with descriptions, images, and links to live demos or GitHub repositories.
+- Provide contact information and links to my social media profiles.
+
+## Installation
+
+To run the website locally, follow these steps:
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/yourusername/your-portfolio-repo.git
+
+2. Navigate to the project directory:
+    ```
+    cd your-portfolio-repo
+    ```
+3. Run index.html in browser
+
+# Contributing
+If you have suggestions for improvements, feel free to open an issue or submit a pull request!
+1. <b>Fork this repository</b>, by clicking on "Fork" button on the top right.
+2. <b>Clone this repository</b>
+   ```
+   git clone https://github.com/<your_username>/Portfolio-website.git
+   ```
+3. <b>Create new branch</b>
+   ```
+   git checkout -b your_branch_name
+   ```
+4. <b>Make changes to the repository.</b>
+5. <b>Commit your changes</b>
+   ```
+   git commit -m "Meaningful message"
+   ```
+6. <b>Push your branch to Github</b>
+   ```
+   git push origin your_branch_name
+   ```
+7. <b>Create Pull Request</b> by clicking on "Compare and Create a Pull request" from your forked repository.
---
+++ script.js
@@ -0,0 +1,69 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+var typed = new Typed(".typing-2", {
+  strings: [
+    " 👨‍💻 A Tech Enthusiast ",
+    "👨‍🎓 An Electronics, Communication and Information Engineering student ",
+    " 🚀 Flutter App Develop",
+    "Backend Developer ",
+    "Data Structure and Algorithm",
+    "Cloud Computing(AWS)",
+  ],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+const myslide = document.querySelectorAll(".myslide"),
+  dot = document.querySelectorAll(".indicator");
+let counter = 1;
+slidefun(counter);
+
+let timer = setInterval(autoSlide, 8000);
+function autoSlide() {
+  counter += 1;
+  slidefun(counter);
+}
+function plusSlides(n) {
+  counter += n;
+  slidefun(counter);
+  resetTimer();
+}
+function currentSlide(n) {
+  counter = n;
+  slidefun(counter);
+  resetTimer();
+}
+function resetTimer() {
+  clearInterval(timer);
+  timer = setInterval(autoSlide, 5000);
+}
+
+function slidefun(n) {
+  let i;
+  for (i = 0; i < myslide.length; i++) {
+    myslide[i].style.display = "none";
+  }
+  for (i = 0; i < dot.length; i++) {
+    dot[i].className = dot[i].className.replace(" active", "");
+  }
+  if (n > myslide.length) {
+    counter = 1;
+  }
+  if (n < 1) {
+    counter = myslide.length;
+  }
+  myslide[counter - 1].style.display = "block";
+  dot[counter - 1].className += " active";
+}
